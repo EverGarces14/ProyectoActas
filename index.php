92411013435f2033401c73d5a19d9e437f8c2a3b
EverG14 <evergarces14@gmail.com>
2024-04-27 17:45:55 -0500

    Ajustes

--- index.php
+++ index.php
@@ -2,10 +2,39 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="Style.css">
+    <title>Inicio de sesion</title>
 </head>
 <body>
-    
+    <form action="IniciarSesion.php" method="POST">
+        <h1>INICIAR SESION</h1>
+        <hr>
+        <?php 
+            if (isset($_GET['error'])) {
+            ?>
+            <p class="error">
+                <?php
+                echo $_GET['error']
+                ?>
+                
+            </p>
+        <?php    
+            }
+        ?>
+
+        <hr>
+        <i class="fa-solid fa-user"></i>
+        <label>Usuario</label>
+        <input type="text" name="Usuario" placeholder="Nombre de usuario">
+
+        <i class="fa-solid fa-unlock"></i>
+        <label>Clave</label>
+        <input type="password" name="Clave" placeholder="Clave">
+        <hr>
+        <button type="submit">Iniciar Sesion</button>
+        <a href="CrearCuenta.php">Crear Cuenta</a>
+    </form>
 </body>
 </html>

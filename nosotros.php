9c9c102e98fc6c750ded428f02cd43161a2746a6
EverG14 <evergarces14@gmail.com>
2024-04-27 13:28:27 -0500

    cambios de titulo

--- nosotros.php
+++ nosotros.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Actas</title>
 </head>
+<h1> Actas de reuniones  del software operatvivo
+</h1>
 <body>
     
 </body>
